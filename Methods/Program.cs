5d8fb45f07a85c6268a90a11f1afc7c167664ccb
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-23 20:29:48 +0300

    C# Dersleri 26: Method Overloading (Yeni Sürümler Desteklemiyor)

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -15,15 +15,26 @@ static int Add3(out int number1, int number2)
     return number1 + number2;
 }
 
+static int Multiply(int number1, int number2)
+{
+    return number1 * number2;
+}
+
+static int Multiply(int number1, int number2, int number3)
+{
+    return number1 * number2 * number3;
+}
+
 //Add();
 //Add();
 //Add();
 //Add();
 //var result = Add2();
 
-int number1;
-int number2 = 100;
-var result2 = Add3(out number1,number2);
+//int number1;
+//int number2 = 100;
+//var result2 = Add3(out number1,number2);
 
-Console.WriteLine(result2);
-Console.WriteLine(number1);
+//Console.WriteLine(result2);
+//Console.WriteLine(number1);
+Console.WriteLine(Multiply(2,4));
