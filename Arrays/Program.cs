cc98b751e0bc67e3ff62d217691ba90fa6587db5
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-23 20:53:06 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] students = new string[3] { "Engin", "Demir", "Salih" };
+students[0] = "Engin";
+students[1] = "Derin";
+students[2] = "Salih";
+
+string[] students2 = { "Engin", "Demir", "Salih" };
+
+foreach (string student in students2)
+{
+    Console.WriteLine(student);
+}
