82b07c7188aba5fca1e413d60b9f32c9d0c34552
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-23 21:29:16 +0300

    C# Dersleri 35: String İle Çalışmaya Başlamak

---
+++ Strings/Program.cs
@@ -0,0 +1,11 @@
+﻿string City = "Ankara";
+//Console.WriteLine(City[0]);
+
+foreach (var item in City)
+{
+    Console.WriteLine(item);
+}
+
+string City2 = "İstanbul";
+
+Console.WriteLine(String.Format("{0} {1}", City,City2));
