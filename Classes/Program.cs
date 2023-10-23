86c205e3e02c10e3ced38e6d50ca4c0db6c7c47e
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-23 21:44:51 +0300

    C# Dersleri 37: Classlara Giriş

---
+++ Classes/Program.cs
@@ -0,0 +1,9 @@
+﻿using Classes;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
