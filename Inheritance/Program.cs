5102a812f0cd7004af3f5edc6cc435295b2d0b4d
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-26 16:17:18 +0300

    C# Dersleri 46: Kalıtıma Giriş

---
+++ Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿Person[] persons = new Person[3]
+{
+    new Customer
+    {
+    FirstName = "Engin"
+    },
+    new Person
+    {
+    FirstName = "Ece"
+    },
+    new Student
+    {
+    FirstName = "Furkan"
+    },
+};
+
+foreach (Person person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+class Person
+{
+    public int id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Departman { get; set; }
+}
