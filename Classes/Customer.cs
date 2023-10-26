4304ff024b18ed6f8b797768bf26828cac8212d8
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-26 15:39:58 +0300

    C# Dersleri 40: Recap Demo Dama Tahtası Yapmak

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -10,17 +10,7 @@ namespace Classes
     {
         //Property
         public int Id { get; set; }
-        private string _firstName;
-        public string FirstName { 
-            get 
-            { 
-                return "Mr. " + _firstName;
-            } 
-            set 
-            { 
-                _firstName = value;
-            } 
-        }
+        public string FirstName {  get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
     }
