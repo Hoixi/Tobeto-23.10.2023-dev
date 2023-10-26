a2954aa96005ed396dc565feddc980791edbc31f
Furkan Kayali <furkankayali0h@gmail.com>
2023-10-26 15:45:30 +0300

    C# Dersleri 41: İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿interface IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+
+}
+
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
